b676c5bbcdbdb9e945817e44403cd494dae047e1
vrtornisiello <vrtornisiello@gmail.com>
2025-04-10 14:59:51 -0300

    updated unit tests

--- chatbot/tests/test_model_factory.py
+++ chatbot/tests/test_model_factory.py
@@ -9,14 +9,14 @@ from chatbot.models import ModelFactory
 def mock_google_cloud_project(monkeypatch):
     monkeypatch.setenv(
         "GOOGLE_CLOUD_PROJECT",
-        "mockup_google_cloud_project"
+        "mock_google_cloud_project"
     )
 
 @pytest.fixture
 def mock_openai_api_key(monkeypatch):
     monkeypatch.setenv(
         "OPENAI_API_KEY",
-        "mockup_openai_api_key"
+        "mock_openai_api_key"
     )
 
 def test_from_model_uri_google(mock_google_cloud_project):
