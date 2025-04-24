c11a1622843f9d7e3a91e2653e22fd0dddc03c90
vrtornisiello <vrtornisiello@gmail.com>
2025-04-24 15:46:53 -0300

    added docstrings

--- chatbot/storage/vector_database.py
+++ chatbot/storage/vector_database.py
@@ -4,6 +4,16 @@ from langchain_openai import OpenAIEmbeddings
 
 
 def get_chroma_vector_store(host: str, port: str | int, collection: str) -> Chroma:
+    """Creates a Chroma vector store from an HTTP client connected to a remote Chroma server
+
+    Args:
+        host (str): The Chroma server hostname.
+        port (str | int): The Chroma server port.
+        collection (str): The name of the Chroma collection.
+
+    Returns:
+        Chroma: An instance of a Chroma vector store.
+    """
     client = chromadb.HttpClient(
         host=host,
         port=port,
@@ -21,6 +31,16 @@ def get_chroma_or_none(
     port: str | int | None,
     collection: str | None
 ) -> Chroma | None:
+    """Returns a Chroma vector store if all parameters are provided, or `None` otherwise.
+
+    Args:
+        host (str | None): The Chroma server hostname.
+        port (str | int | None): The Chroma server port.
+        collection (str | None): The name of the Chroma collection.
+
+    Returns:
+        Chroma | None: An instance of a Chroma vector store, or `None`.
+    """
     if host and port and collection:
         return get_chroma_vector_store(host, port, collection)
     return None
