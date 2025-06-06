544a67940a9f05306a54965a4b84eb343131dc08
vrtornisiello <vrtornisiello@gmail.com>
2025-06-06 14:18:36 -0300

    add formatting functions to the `__init__.py` file of the assistants module

--- chatbot/assistants/__init__.py
+++ chatbot/assistants/__init__.py
@@ -1,6 +1,7 @@
 from .assistants import Assistant, AsyncAssistant
 from .async_sql_assistant import AsyncSQLAssistant
 from .async_sql_viz_assistant import AsyncSQLVizAssistant
+from .formatting import format_router_agent_response, format_sql_agent_response
 from .messages import SQLAssistantMessage, SQLVizAssistantMessage
 from .sql_assistant import SQLAssistant
 from .sql_viz_assistant import SQLVizAssistant
