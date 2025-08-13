75906cdc336fa0d1b60c05c0d189db18307d84f4
vrtornisiello <vrtornisiello@gmail.com>
2025-08-13 14:35:54 -0300

    create custom reducer for chat turns

--- chatbot/agents/reducers.py
+++ chatbot/agents/reducers.py
@@ -14,15 +14,26 @@ class Item(BaseItem):
 class ItemRemove(BaseItem):
     ...
 
+class BaseChatTurn(BaseModel):
+    id: int # easier for the LLMs to parse
+
+class ChatTurn(BaseChatTurn):
+    user_question: str
+    ai_response: str
+    data: list[Item]
+
+class ChatTurnRemove(BaseChatTurn):
+    ...
+
 def add_item(existing: Item|list[Item], update: BaseItem|list[BaseItem]) -> list[Item]:
     """Merges two lists of Items, deleting or updating Items by ID.
 
     Args:
-        existing (list[Item]): A base list of Item objects
-        update (list[BaseItem]): A list of Item or ItemRemove objects
+        existing (Item | list[Item]): An Item or a list of Item objects.
+        update (BaseItem | list[BaseItem]): A BaseItem or a list of BaseItem objects.
 
     Returns:
-        list[Item]: The merged list
+        list[Item]: The merged list.
     """
     if not isinstance(existing, list):
         existing = [existing]
@@ -31,7 +42,7 @@ def add_item(existing: Item|list[Item], update: BaseItem|list[BaseItem]) -> list
         update = [update]
 
     # copying to avoid modifying the existing list
-    merged: list[Item] = existing.copy() or []
+    merged: list[Item] = existing.copy()
 
     ids_to_idx = {item.id: idx for idx, item in enumerate(existing)}
 
@@ -53,5 +64,33 @@ def add_item(existing: Item|list[Item], update: BaseItem|list[BaseItem]) -> list
             merged.append(item)
 
     merged = [item for item in merged if item.id not in ids_to_remove]
+
+    return merged
+
+def add_chat_turn(existing: dict[int, ChatTurn], update: dict[int, BaseChatTurn]) -> dict[int, ChatTurn]:
+    """Merges two dictionaries of Chat Turns, deleting or updating Chat Turns by key.
+
+    Args:
+        existing (dict[int, ChatTurn]): A dictionary of ChatTurn objects.
+        update (dict[int, BaseChatTurn]): A dictionary of BaseChatTurn objects.
+
+    Returns:
+        dict[int, ChatTurn]: The merged dictionary.
+    """
+    merged = existing.copy()
+
+    for k, v in update.items():
+        if isinstance(v, ChatTurnRemove):
+            if k not in merged:
+                raise ValueError(
+                    f"Attempted to delete non-existent chat turn '{k}'"
+                )
+            del merged[k]
+        elif isinstance(v, ChatTurn):
+            merged[k] = v
+        else:
+            raise TypeError(
+                f"Expected `ChatTurn` or `ChatTurnRemove`, got '{type(v)}'"
+            )
 
     return merged
