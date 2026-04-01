29e04ec55dd207ba370e62a1dd6340a5967aea85
vrtornisiello <vrtornisiello@gmail.com>
2026-04-01 17:27:53 -0300

    fix: remove accidentally committed file

--- sanitize.py
+++
@@ -1,7 +0,0 @@
-from app.api.streaming import _sanitize_markdown_links
-
-safe_markdown = _sanitize_markdown_links(
-    "Check [this](https://basedosdados.com) and [that](https://basedosdados.org)"
-)
-
-print(safe_markdown)
